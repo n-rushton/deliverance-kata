7902848f184395b41bd3fc1e800628e07c6e0d13
Nicola <nicola_rushton@outlook.com>
2022-05-04 17:13:13 +0100

    green

--- src/main/java/codurance/kata/PostageCalculator.java
+++ src/main/java/codurance/kata/PostageCalculator.java
@@ -2,6 +2,10 @@ package codurance.kata;
 
 public class PostageCalculator {
     public Money parcelPricing(int weight, int height, int width, int depth, Currency currency) {
+        if (weight > 60) {
+            return new Money(currency, weight * 4 * 100);
+        }
         return new Money(currency, 12_000);
+
     }
 }
--- src/test/java/codurance/kata/PostageCalculatorTest.java
+++ src/test/java/codurance/kata/PostageCalculatorTest.java
@@ -6,17 +6,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PostageCalculatorTest {
 
+    private final int LOWER_TIER_HEIGHT = 229;
+    private final int LOWER_TIER_WIDTH = 162;
+    private final int LOWER_TIER_DEPTH = 25;
+
+
+    private final int LOWER_TIER_WEIGHT = 60;
+    private final int MID_TIER_WEIGHT = 500;
+
     @Test
     public void apply_small_parcel_pricing_to_a_package_with_lower_tier_dimensions_and_weight() {
 
         Money expectedCost = new Money(Currency.GBP, 12_000);
-        PostageCalculator postageCalculator = new PostageCalculator();
-        int weight = 60;
-        int height = 229;
-        int width = 162;
-        int depth = 25;
-        Money postageCost = postageCalculator.parcelPricing(weight, height, width, depth, Currency.GBP);
-        assertEquals(expectedCost, postageCost);
 
+        PostageCalculator postageCalculator = new PostageCalculator();
+        Money postageCost = postageCalculator.parcelPricing(LOWER_TIER_WEIGHT, LOWER_TIER_HEIGHT, LOWER_TIER_WIDTH, LOWER_TIER_DEPTH, Currency.GBP);
+
+        assertEquals(expectedCost, postageCost);
+    }
+
+    @Test
+    public void apply_mid_parcel_pricing_to_a_package_with_lower_tier_dimensions_but_mid_tier_weight() {
+
+        int expectedPriceInPence = MID_TIER_WEIGHT * 4 * 100;
+        Money expectedCost = new Money(Currency.GBP, expectedPriceInPence);
+
+        PostageCalculator postageCalculator = new PostageCalculator();
+        Money postageCost = postageCalculator.parcelPricing(MID_TIER_WEIGHT, LOWER_TIER_HEIGHT, LOWER_TIER_WIDTH, LOWER_TIER_DEPTH, Currency.GBP);
+
+        assertEquals(expectedCost, postageCost);
     }
 }
