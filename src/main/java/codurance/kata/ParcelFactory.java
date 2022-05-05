5242b098889fcbd1f39aa3bf66c876000a14d796
Nicola <nicola_rushton@outlook.com>
2022-05-05 11:26:26 +0100

    factory refactor

---
+++ src/main/java/codurance/kata/ParcelFactory.java
@@ -0,0 +1,29 @@
+package codurance.kata;
+
+public class ParcelFactory {
+
+    public ParcelFactory() {
+    }
+
+    public Parcel createParcel(int weight, int height, int width, int depth) {
+        if (isLargeParcel(weight, height, width, depth)) {
+            return new LargeParcel(weight, height, width, depth);
+        }
+
+        if (isMediumParcel(weight, height, width, depth)) {
+            return new MediumParcel(weight, height, width, depth);
+        }
+
+        return new SmallParcel(weight, height, width, depth);
+    }
+
+
+    private boolean isMediumParcel(int weight, int height, int width, int depth) {
+        return weight > 60 || height > 229 || width > 162 || depth > 25;
+    }
+
+    private boolean isLargeParcel(int weight, int height, int width, int depth) {
+        return weight > 500 || height > 324 || width > 229 || depth > 100;
+    }
+
+}
--- src/main/java/codurance/kata/PostageCalculator.java
+++ src/main/java/codurance/kata/PostageCalculator.java
@@ -5,28 +5,19 @@ import java.math.BigDecimal;
 import static java.lang.Math.max;
 
 public class PostageCalculator {
+
+    ParcelFactory parcelFactory = new ParcelFactory();
+
+
     public Money parcelPricing(int weight, int height, int width, int depth, Currency currency) {
-        BigDecimal parcelCost = new SmallParcel(weight, height, width, depth).calculate();
+        Parcel parcel = parcelFactory.createParcel(weight, height, width, depth);
 
-
-        if (isLargeParcel(weight, height, width, depth)) {
-            parcelCost = new LargeParcel(weight, height, width, depth).calculate();
-        }
-
-        else if (isMediumParcel(weight, height, width, depth)) {
-            parcelCost =  new MediumParcel(weight, height, width, depth).calculate();
-        }
+        BigDecimal parcelCost = parcel.calculate();
 
         return new Money(currency, parcelCost);
 
     }
 
-    private boolean isMediumParcel(int weight, int height, int width, int depth) {
-        return weight > 60 || height > 229 || width > 162 || depth > 25;
-    }
 
-    private boolean isLargeParcel(int weight, int height, int width, int depth) {
-        return weight > 500 || height > 324 || width > 229 || depth > 100;
-    }
 
 }
